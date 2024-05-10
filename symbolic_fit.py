dd5c3bd5525b5fdec363d59bb5f474146370ee34
sdaloglu <107580744+sdaloglu@users.noreply.github.com>
2024-05-10 23:08:12 +0100

    Transferred the python notebook to a python script

--- symbolic_fit.py
+++ symbolic_fit.py
@@ -1,22 +1,47 @@
 # This script is for fitting a symbolic regression model to the data
 
 from pysr import PySRRegressor 
+import pickle as pkl
+import pandas as pd
+import numpy as np
 
-
-default_pysr_params = dict(
-    populations = 30,
-    model_selection = "best",
-)
 
 
 # Create the symbolic regression model and train it
 model = PySRRegressor(
     niterations=1000,
+    popoulations = 30
     binary_operators=["+", "-", "*", "/"],
     unary_operators=["sin", "cos", "exp", "log", "abs", "sqrt"],
-    **default_pysr_params
 )
 
 
-model.fit(X_train, y_train)
+# Load the message data from the trained model - this also includes the node embedding for receiving and sending nodes
+messages_over_time = pkl.load(open("models/messages_over_time.pkl", "rb"))
+
+# Select the last element of the list corresponding to the final epoch
+last_message = messages_over_time[-1]
+
+
+# Find the most significant elements of the learned message vector
+# Calculate the two with the largest standard deviation
+
+# Calculate the standard deviations for each set of values
+std_deviations = [np.std(messages_over_time[-1]['e%d' % i]) for i in range(100)]
+
+# Sort the standard deviations in descending order and get the indices of the first two elements
+best_indices = np.argsort(std_deviations)[::-1][:2]
+
+# Extract the best messages
+best_messages = last_message[['e%d'%(best_indices[0],), 'e%d'%(best_indices[1],), 'dx', 'dy', 'r', 'm1', 'm2']]
+
+# Now, let's fir e15 and e48 as a function of dx, dy, r, m1, and m2
+
+F1 = best_messages['e%d'%(best_indices[0],)]
+F2 = best_messages['e%d'%(best_indices[1],)]
+
+# First fit the highest standard deviation element
+model.fit(best_messages[['dx', 'dy', 'r', 'm1', 'm2']], F1)
+
+# PySR automatically saves the symbolic equation
 
