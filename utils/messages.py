6678b973224ad5b8b67a4b31f3cc7cbe0d562947
Sabahattin Mert Daloglu <smd89@cam.ac.uk>
2024-06-19 23:35:39 +0100

    Documented the script and each function/class

--- utils/messages.py
+++ utils/messages.py
@@ -1,28 +1,31 @@
-# This file contains the function to organize and record the learned messages over time
+"""
+This file contains the function to organize and record the learned messages over time.
+"""
 
 import numpy as np
 import sys
 import pandas as pd
-sys.path.append('utils')    # Add the utils directory to the path
+import torch
 from my_models import loss_function, GN
-import torch 
 
-# We want to check if the learned model has messages that correspond to the true force of the simulated system
+# Add the utils directory to the path
+sys.path.append('utils')
 
-# Define a function to extract the messages from the model
-
-def get_messages(model, test_loader, msg_dim, dim = 2):
+def get_messages(model, test_loader, msg_dim, dim=2):
     """
-    Args:
-        model (torch.nn.Module): The model to extract the messages from
+    Extracts messages from the model.
 
-        
+    Args:
+        model (torch.nn.Module): The model to extract the messages from.
+        test_loader (DataLoader): DataLoader for the test dataset.
+        msg_dim (int): Dimension of the messages.
+        dim (int, optional): Dimensionality of the data (2 or 3). Defaults to 2.
+
     Returns:
-        messages (np.array): The messages extracted from the model
+        pd.DataFrame: The messages extracted from the model.
     """
     
-    # Move the model to the CPU
-    #model.cpu()
+    model.eval()
     
     # Initialize an empty list to store the messages
     messages = []
@@ -30,51 +33,51 @@ def get_messages(model, test_loader, msg_dim, dim = 2):
     for batch in test_loader:
         
         # Extract the node features of the source nodes
-        x_source = batch.x[batch.edge_index[0]]   # We want the graph connectivity info for all graphs in the batch, hence the batch.edge_index[0]
+        x_source = batch.x[batch.edge_index[0]]  # Graph connectivity info for all graphs in the batch
         
         # Extract the node features of the target nodes
         x_target = batch.x[batch.edge_index[1]] 
         
         # Get the messages
-        if msg_dim == 200:    # KL regularization
-            message = model.edge_model(torch.cat([x_source, x_target], dim = 1))
-            mean = message[:,:100]
-            message = mean
-            
-        else:    # L1 regularization, bottleneck, or no regularization (Standard)
-            message = model.edge_model(torch.cat([x_source, x_target], dim = 1))
-        
+        if msg_dim == 200:  # KL regularization
+            message_mu_log_var = model.edge_model(torch.cat([x_source, x_target], dim=1))
+            mu = message_mu_log_var[:, 0::2]
+            log_var = message_mu_log_var[:, 1::2]
+            message = torch.cat([mu, log_var], dim=1)
+        else:  # L1 regularization, bottleneck, or no regularization (Standard)
+            message = model.edge_model(torch.cat([x_source, x_target], dim=1))
         
         # Append the node features to the messages list
-        message_with_node_features = torch.cat((x_source,x_target,message), dim = 1)
+        message_with_node_features = torch.cat((x_source, x_target, message), dim=1)
         
-        
-        if msg_dim == 200: # KL regularization
+        # Define columns based on dimensionality and message dimension
+        if msg_dim == 200:  # KL regularization
             if dim == 2:
-                columns = [elem%(k) for k in range(1, 3) for elem in 'x%d y%d vx%d vy%d q%d m%d'.split(' ')]
-                columns += ['e%d'%(k,) for k in range(100)]
+                columns = [f'{elem}{k}' for k in range(1, 3) for elem in 'x y vx vy q m'.split()]
+                columns += [f'e{k}' for k in range(100)]
+                columns += [f'log_var{k}' for k in range(100)]
             elif dim == 3:
-                columns = [elem%(k) for k in range(1, 3) for elem in 'x%d y%d z%d vx%d vy%d vz%d q%d m%d'.split(' ')]
-                columns += ['e%d'%(k,) for k in range(100)]
+                columns = [f'{elem}{k}' for k in range(1, 3) for elem in 'x y z vx vy vz q m'.split()]
+                columns += [f'e{k}' for k in range(100)]
+                columns += [f'log_var{k}' for k in range(100)]
         else:
             if dim == 2:
-                columns = [elem%(k) for k in range(1, 3) for elem in 'x%d y%d vx%d vy%d q%d m%d'.split(' ')]
-                columns += ['e%d'%(k,) for k in range(msg_dim)]
+                columns = [f'{elem}{k}' for k in range(1, 3) for elem in 'x y vx vy q m'.split()]
+                columns += [f'e{k}' for k in range(msg_dim)]
             elif dim == 3:
-                columns = [elem%(k) for k in range(1, 3) for elem in 'x%d y%d z%d vx%d vy%d vz%d q%d m%d'.split(' ')]
-                columns += ['e%d'%(k,) for k in range(msg_dim)]
-
+                columns = [f'{elem}{k}' for k in range(1, 3) for elem in 'x y z vx vy vz q m'.split()]
+                columns += [f'e{k}' for k in range(msg_dim)]
         
-        # List of messages -- Pandas dataframe
+        # Append the messages to the list as a Pandas DataFrame
         messages.append(pd.DataFrame(
             data=message_with_node_features.cpu().detach().numpy(),
             columns=columns
         ))
         
+    # Concatenate all message DataFrames
     messages = pd.concat(messages)
     
-    # Adding the extra information to the messages dataframe --> delta x and delta y and r
-    # These information are used for calculating the true force
+    # Adding extra information to the messages DataFrame: delta x, delta y, and r
     messages['dx'] = messages.x1 - messages.x2
     messages['dy'] = messages.y1 - messages.y2
     
@@ -89,12 +92,6 @@ def get_messages(model, test_loader, msg_dim, dim = 2):
             (messages.dx)**2 + (messages.dy)**2 + (messages.dz)**2
         )
     
+    model.train()
+    
     return messages
-
-        
-
-        
-        
-        
-        
-        
